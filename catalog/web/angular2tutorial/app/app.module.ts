d0a23b05c068dedbbc0770490f174f65774942da
ASM-SE <andersonmarcolino@gmail.com>
2016-11-30 21:50:37 -0200

    quickStart guide to angular2 router #7

--- catalog/web/angular2tutorial/app/app.module.ts
+++ catalog/web/angular2tutorial/app/app.module.ts
@@ -1,18 +1,49 @@
 import { NgModule }      from '@angular/core';
 import { FormsModule }   from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
-  imports:      [ 
+  imports: [ 
     BrowserModule,
-    FormsModule
-     ],
-  declarations: [ AppComponent,
-                  HeroDetailComponent ],
-  bootstrap:    [ AppComponent ]
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'detail/:id',
+        component: HeroDetailComponent
+      }
+    ])
+  ],
+  declarations: [ 
+    AppComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent
+  ],
+  providers: [ 
+    HeroService //colocando o service agui ele se torna uma instancia do tipo singleton
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
+
--- catalog/web/angular2tutorial/app/hero-detail.component.ts
+++ catalog/web/angular2tutorial/app/hero-detail.component.ts
@@ -1,23 +1,33 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params }   from '@angular/router';
+import { Location }                 from '@angular/common';
+import 'rxjs/add/operator/switchMap';
 
-import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
+    moduleId: module.id,
     selector: 'my-hero-detail',
-    template: `
-        <div *ngIf="hero">
-            <h2>{{hero.name}} details!</h2>
-            <div><label>id: </label>{{hero.id}}</div>
-            <div>
-                <label>name: </label>
-                <input [(ngModel)]="hero.name" placeholder="name here" />
-            </div>
-        </div>
-            
-               `
+    templateUrl: 'hero-detail.component.html'
 })
 
-export class HeroDetailComponent {
+export class HeroDetailComponent implements OnInit {
+
+    constructor(
+        private heroService: HeroService,
+        private route: ActivatedRoute,
+        private location: Location
+    ) {}
+
+    ngOnInit(): void {
+        this.route.params
+            .switchMap((params: Params) => this.heroService.getHero(+params['id'])) //The hero id is a number. Route parameters are always strings. So we convert the route parameter value to a number with the JavaScript (+) operator.
+            .subscribe(hero => this.hero = hero); //The subscriptions are cleaned up when the component is destroyed, protecting against memory leaks, so we don't need to unsubscribe from the route params Observable
+    }
+
+    goBack(): void {  //Going back too far could take us out of the application. That's acceptable in a demo. We'd guard against it in a real application, perhaps with the CanDeactivate guard.
+        this.location.back();
+    }
 
   @Input()
   hero: Hero;
