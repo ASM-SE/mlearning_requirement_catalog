4dcc674950ab7a23c902844d3b788601e169257d
ASM-SE <andersonmarcolino@gmail.com>
2016-11-30 10:31:05 -0200

    quickStart guide to angular2 #3

--- catalog/web/angular2tutorial/app/app.module.ts
+++ catalog/web/angular2tutorial/app/app.module.ts
@@ -1,10 +1,16 @@
 import { NgModule }      from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppComponent }  from './app.component';
+import { Hero } from './hero';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ 
+    BrowserModule,
+    FormsModule
+     ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
