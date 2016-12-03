a5ca4703c3a265224f552db68044523ff3c69fd0
ASM-SE <andersonmarcolino@gmail.com>
2016-12-03 15:22:06 -0200

    InMemoryDbService angular 2 tutorial #15

--- catalog/web/angular2tutorial/app/hero.service.ts
+++ catalog/web/angular2tutorial/app/hero.service.ts
@@ -22,6 +22,11 @@ import { Hero } from './hero';
                 .catch(this.handleError);
         }
 
+        private handleError(error: any): Promise<any> {
+        console.error('An error occurred', error); // for demo purposes only
+        return Promise.reject(error.message || error);
+        }        
+
         getHeroesSlowly(): Promise<Hero[]> {
         return new Promise<Hero[]>(resolve =>
             setTimeout(resolve, 2000)) // delay 2 seconds
