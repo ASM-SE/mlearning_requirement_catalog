cf5965959bea47b1401bab1ff8cef4eab0d66370
ASM-SE <andersonmarcolino@gmail.com>
2016-12-03 15:16:37 -0200

    HTTP angular 2 tutorial #15

--- catalog/web/angular2tutorial/app/hero.service.ts
+++ catalog/web/angular2tutorial/app/hero.service.ts
@@ -1,14 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+//import { HEROES } from './mock-heroes';
 
 @Injectable()
     export class HeroService {
-        getHeroes(): Promise<Hero[]> {
+    /*    getHeroes(): Promise<Hero[]> {
            return Promise.resolve(HEROES); 
-        } 
+        }*/
 
+  private heroesUrl = 'app/heroes';  // URL to web api
+
+  constructor(private http: Http) { }         
+
+        getHeroes() : Promise<Hero[]> {
+            return this.http.get(this.heroesUrl)
+                .toPromise()
+                .then(response => response.json().data as Hero[])
+                .catch(this.handleError);
+        }
 
         getHeroesSlowly(): Promise<Hero[]> {
         return new Promise<Hero[]>(resolve =>
