fc5620fe7e819a8f8a41b874bd561a4daecb2d2f
ASM-SE <andersonmarcolino@gmail.com>
2016-12-06 17:43:34 -0200

    adding angular material angular 2 #3

--- catalog/web/app/app-routing.module.ts
+++ catalog/web/app/app-routing.module.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
     {
         path: 'requirement',
         component: RequirementsListComponent
-    },
+    }
 ];
 
 @NgModule({
--- catalog/web/app/app.component.ts
+++ catalog/web/app/app.component.ts
@@ -3,30 +3,37 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: ` 
-    <md-toolbar class="md-hue-2">
-      <div class="md-toolbar-tools">
 
-        <h2>
-          <span>{{title}}</span>
-        </h2>
-        <span flex=""></span>
-
-      </div>
-    </md-toolbar>
-  
-  
-  
-  <div layout="row" layout-xs="column">
-            <h1>{{title}}</h1>
-             <nav>
+<md-sidenav-layout>
+  <md-sidenav #sidenav mode="side" class="app-sidenav">
+     <nav>
               <a routerLink="/requirement" routerLinkActive="active">Requirement</a>
             </nav>
+  </md-sidenav>
+
+ <md-toolbar color="primary">
+    <button class="app-icon-button" (click)="sidenav.toggle()">
+      <i class="material-icons app-toolbar-menu">menu</i>
+    </button>
+
+    Angular Material2 Example App
+
+    <span class="app-toolbar-filler"></span>
+  
+  </md-toolbar>
+  </md-sidenav-layout>
+
+
+
             <router-outlet></router-outlet>
-            </div>
+          
     `,
+  styleUrls: ['/app.component.css'],
 })
 export class AppComponent  { 
   title = 'M-learning Requirement Catalog'; 
 }
+
+
 
 
