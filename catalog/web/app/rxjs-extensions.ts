fe77cda3e35410097376ecc210d30661933701a2
ASM-SE <andersonmarcolino@gmail.com>
2016-12-09 23:21:14 -0200

    rxjs

--- catalog/web/app/rxjs-extensions.ts
+++ catalog/web/app/rxjs-extensions.ts
@@ -10,3 +10,5 @@ import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/switchMap';
+
+//More to study: https://github.com/Reactive-Extensions/RxJS
