fe77cfea88eaa0766c819aec6f3512b2b6dc14f9
ASM-SE <andersonmarcolino@gmail.com>
2016-12-14 09:46:58 -0200

    Remove unused components

---
+++ catalog/web/app/components/modal.component.ts
@@ -0,0 +1,23 @@
+import {MODAL_DIRECTVES, BS_VIEW_PROVIDERS} from 'ng2-bootstrap/ng2-bootstrap';
+import {Component, ViewChild, ElementRef} from "@angular/core";
+
+@Component({
+    selector: 'custom-modal',
+    moduleId: module.id,
+    templateUrl: './modal.component.html',
+    styleUrls: ['./modal.component.css'],
+   // directives: [MODAL_DIRECTVES],
+    viewProviders: [BS_VIEW_PROVIDERS]
+})
+
+export class ModalComponent {
+    modalBody:string;
+    @ViewChild('lgModal') modal:ElementRef;
+
+    showModal(modalBody:string) {
+        if (modalBody != undefined) {
+            this.modalBody = modalBody;
+            this.modal.show();
+        }
+    }
+}
