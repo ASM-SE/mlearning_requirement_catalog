c15920fc2a62429e38e79f54d26ce96e1b8ad744
ASM-SE <andersonmarcolino@gmail.com>
2016-12-16 22:49:21 -0200

    app.component.html

--- catalog/web/app/app.component.ts
+++ catalog/web/app/app.component.ts
@@ -3,10 +3,10 @@ import { Component,OnInit } from '@angular/core';
 
 
 @Component({
+  moduleId: module.id,  
   selector: 'my-app',
   templateUrl: 'app.component.html',
-  styleUrls: ['/app.component.css']
-
+  styleUrls: ['/app.component.css'],
 })
 export class AppComponent  { 
   title = 'M-learning Requirement Catalog'; 
