5f41773d8c745db34fc4d03775b29efd54a07f5a
ASM-SE <andersonmarcolino@gmail.com>
2016-12-16 22:41:55 -0200

    split app.component.ts

--- catalog/web/app/app.component.ts
+++ catalog/web/app/app.component.ts
@@ -4,49 +4,7 @@ import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: ` 
-<md-sidenav-layout  fullscreen> <!--Ddeve ser fechado até onde o sidenav deve sobrepor-->
-      
-        <md-sidenav #left mode="over" layout-padding>
-
-        <md-toolbar color="primary">
-
-              
-            <h2>Menu</h2>
-        
-        </md-toolbar>        
-
-<md-nav-list> 
-<a md-list-item md-ink-ripple="#333333" routerLink="/requirement" routerLinkActive="active" (click)="left.toggle()">
- <md-icon md-list-icon>palette</md-icon>
-  <span md-line>Requirement</span> </a> 
-    </md-nav-list>           
-
-         <a routerLink="/requirement" routerLinkActive="active">Requirement</a>
-
-        </md-sidenav>
-
-        <md-toolbar color="primary">
-
-              <button md-icon-button class="app-icon-button" (click)="left.toggle()">
-                <md-icon>menu</md-icon>
-              </button>
-
-            Toolbar with Icon Buttons
-        
-        </md-toolbar>
-
-
-
-
-        <div class="app-content">
-                    <router-outlet></router-outlet>
-        </div>
-    
-    </md-sidenav-layout>
-
-
-    `,
+  templateUrl: 'app.component.html',
   styleUrls: ['/app.component.css']
 
 })
